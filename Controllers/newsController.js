fe9e4774daef539054908fd3c33b99de41fa83e9
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-01-05 22:36:19 +0600

    postNews

--- Controllers/newsController.js
+++ Controllers/newsController.js
@@ -5,10 +5,16 @@ const getAllNews = async (req, res) => {
   return res.json(news);
 };
 
-const getOneNews = async (req, res) => {
+const getTargetNews = async (req, res) => {
   const news = await QueryNews(req.params.id);
   if (!news) {
     return res.status(404).send({ message: "data not found" });
   }
   return res.send(news);
+};
+
+const postNews = async (req, res) => {
+  const data = req.body;
+  const result = await QueryPost(data);
+  return res.json(result);
 };
